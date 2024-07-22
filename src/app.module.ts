374c9e1c7f649a66b86955a4e547527c9d37f506
JeanSousa <jeanjr.silvasousa@gmail.com>
2024-07-22 01:39:01 -0300

    Feature/adicao-controller-repository-module-de-produtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
   // estou importando um outro modulo no app.module o modulo raiz
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, ProdutoModule],
 })
 export class AppModule {}
---
+++ src/produto/produto.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProdutoRepository } from './produto.repository';
+
+@Controller('/produtos')
+export class ProdutoController {
+  constructor(private produtoRepository: ProdutoRepository) {}
+
+  @Post()
+  async criarProduto(@Body() dadosProduto) {
+    this.produtoRepository.salvar(dadosProduto);
+    return dadosProduto;
+  }
+
+  @Get()
+  async listarProdutos() {
+    return this.produtoRepository.listar();
+  }
+}
